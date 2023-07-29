44ec0b51e2c7b77086e78c0ef5a44b763758a83c
RobMxz <robmxz1234@gmail.com>
2023-07-28 20:51:56 -0500

    Second Commit

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("hello world")
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf 
+import numpy as np
+import matplotlib.pyplot as plt
+
+celsius = np.array([-40,-10,0,8,15,22,38],dtype=float)
+fahrenheit = np.array([-40,14,32,46,59,72,100],dtype=float)
+
+#capa = tf.keras.layers.Dense(units=1, input_shape=[1])
+#modelo = tf.keras.Sequential([capa])
+
+oculta1 = tf.keras.layers.Dense(units=3, input_shape=[1])
+oculta2 = tf.keras.layers.Dense(units=3)
+salida = tf.keras.layers.Dense(units=1)
+modelo = tf.keras.Sequential([oculta1,oculta2,salida])
+
+modelo.compile(
+    optimizer = tf.keras.optimizers.Adam(0.1),
+    loss='mean_squared_error'
+)
+print("Entrenando ...")
+historial = modelo.fit(celsius,fahrenheit, epochs=1000, verbose = False)
+print("Modelo entrenado")
+
+plt.xlabel("# Epoca")
+plt.ylabel("Magnitud de pérdida")
+plt.plot(historial.history["loss"])
+plt.show()
+
+print("Hagamos una prediccion!")
+resultado = modelo.predict([100.0])
+print(f'El resutado es {resultado} fahrenheit')
+
+print("Variables internas del modelo")
+print(oculta1.get_weights())
+print(oculta2.get_weights())
+print(salida.get_weights())
